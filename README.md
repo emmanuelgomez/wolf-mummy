436a886bb48bf9127f99b40ca6a20e9e587d04f2
Emmanuel Gomez Soleremmanuel.gomezsoler@gmail.com <Emmanuel Gomez Soleremmanuel.gomezsoler@gmail.com>
2019-03-04 23:40:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# django-todo
+This is a simple to-do list application divided in two parts: an API and a Web Application client.
+
+---
+
+#### Technologies
+- Vue.js 2.5.2
+- Python 3.6.4
+- Django 2.0.2
+- django-rest-framework 3.7.7
+- PostgreSQL 10.2
+
+#### Running with Docker
+
+Building app image:
+```sh
+    docker-compose build
+```
+
+Setting the database up:
+```sh
+    docker-compose run api python manage.py makemigrations
+    docker-compose run api python manage.py migrate
+```
+
+Installing npm modules:
+```sh
+    docker-compose run web npm install
+```
+
+Cleaning up:
+```sh
+    docker-compose down
+    docker-compose rm -f --all
+```
+
+Running the app:
+```sh
+    docker-compose up
+```
+
+Access the browsable API at [http://localhost:8000](http://localhost:8000).  
+Access the web client at [http://localhost:8080](http://localhost:8080).
+
+Bring the app down:
+```sh
+    docker-compose down
+```
---
+++ api/django_mummy/core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils.text import Truncator
+from django.core.validators import MaxValueValidator, MinValueValidator
+from math import log, floor
+
+class Todo(models.Model):
+    text = models.CharField(max_length=50, blank=False)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return Truncator(self.message).chars(20)
+
+
+class Investor(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+    childrens = parent.children_set()
+    innocence = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    experience = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    charisma = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    money = models.FloatField()
+
+    def GetProbabilityToFindInvestors(self):
+        return self.experience * self.charisma * (1 - log(len(self.childrens),10))
+
+    def GetProbabilityCandidateAccepts(self):
+        return self.innocence * (1-self.experience)
+
+    def GetMaximumWeeksWithoutRecovery(self):
+        return floor(1 - self.innocence) * self.experience * self.charisma * 10
