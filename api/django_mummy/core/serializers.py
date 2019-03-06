775b7b3567a82ffeb979879e8bd5f01b9ad82c2c
Emmanuel Gomez <emmanuel.gomezsoler@gmail.com>
2019-03-06 01:52:39 -0300

    added api methods to return tree structure

--- api/django_mummy/core/serializers.py
+++ api/django_mummy/core/serializers.py
@@ -10,3 +10,29 @@ class InvestorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investor
         fields = '__all__'
+
+# class MummyInvestorsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Investor
+#         fields = ('innocence', 'experience','charisma','money' )
+
+# class MummySerializer(serializers.ModelSerializer):
+#     parent = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+#     # mummyInvestors = serializers.MummyInvestorsSerializer()
+#
+#     class Meta:
+#         model = Investor
+#         fields = ('parent', 'innocence', 'experience','charisma','money')
+
+
+class RecursiveSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+class MummySerializer(serializers.ModelSerializer):
+    children = RecursiveSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Investor
+        fields = ('id',  'innocence', 'experience','charisma','money', 'parent', 'children')
--- api/django_mummy/core/views.py
+++ api/django_mummy/core/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-
+from rest_framework.decorators import list_route
 from .models import Todo, Investor
-from .serializers import TodoSerializer,InvestorSerializer
+from .serializers import TodoSerializer,InvestorSerializer,MummySerializer
 
 
 class APIRoot(APIView):
@@ -17,7 +18,8 @@ class APIRoot(APIView):
     def get(self, request):
         return Response({
             'todos': reverse('todo-list-view', request=request),
-            'investors': reverse('investors-list-view', request=request)
+            'investors': reverse('investors-list-view', request=request),
+            'dashboard': reverse('dashboard-view', request=request)
         })
 
 class InvestorList(APIView):
@@ -36,51 +38,48 @@ class InvestorList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-class TodoList(APIView):
+class InvestorItem(APIView):
     """
-    List all to-dos or create a new one.
-    """
-    def get(self, request, format=None):
-        todos = Todo.objects.all()
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class TodoItem(APIView):
-    """
-    Retrieve, delete, update or update partially a to-do instance.
+    Retrieve, delete, update or update partially a investor instance.
     """
     def get(self, request, pk, format=None):
-        todo = get_object_or_404(Todo, id=pk)
-        serializer = TodoSerializer(todo)
+        investor = get_object_or_404(Investor, id=pk)
+        serializer = InvestorSerializer(investor)
         return Response(serializer.data)
 
     def patch(self, request, pk, format=None):
-        todo = get_object_or_404(Todo, id=pk)
-        serializer = TodoSerializer(todo, data=request.data, partial=True)
+        investor = get_object_or_404(Investor, id=pk)
+        serializer = InvestorSerializer(investor, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        todo = get_object_or_404(Todo, id=pk)
-        todo.delete()
+        investor = get_object_or_404(Investor, id=pk)
+        investor.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def put(self, request, pk, format=None):
-        todo = get_object_or_404(Todo, id=pk)
-        serializer = TodoSerializer(todo, data=request.data)
+        investor = get_object_or_404(Investor, id=pk)
+        serializer = InvestorSerializer(investor, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DashboardItem(APIView):
+    """
+        Dashboard methods
+        """
+    queryset = Investor.objects.all()
+
+    serializer_class = MummySerializer
+
+    @list_route()
+    def get(self, request):
+        queryset = Investor.objects.filter(parent=None)
+        serializer = MummySerializer(queryset, many=True)
+        return Response({serializer.data})
+
+
